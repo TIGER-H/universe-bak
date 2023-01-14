337fea394c0a66236bcbba55a5904e71d9ab3a83
Yeoreum <549747168@qq.com>
2023-01-15 00:26:55 +0800

    generate link

--- link.ts
+++ link.ts
@@ -9,11 +9,29 @@ const generateLink = () => {
       Deno.readTextFileSync("./original/contents/" + file.name),
     );
     //@ts-ignore any
-    const link = contents.map((content) => content.path);
-    Deno.mkdirSync(`./link/original/${name}`);
+    const links = contents.map((content) => content.previewPath) as string[];
+
+    //change to original link
+    const convertedLinks = links.map((link) => {
+      // before
+      // https://d1ksgh4vgfm8hg.cloudfront.net/converted/5/3/4/32553025-07c0-4ab5-b173-f09141b887cf.mp4/preview/32553025-07c0-4ab5-b173-f09141b887cfmaster.m3u8
+      // after
+      // https://d1ksgh4vgfm8hg.cloudfront.net/5/3/4/32553025-07c0-4ab5-b173-f09141b887cf.mp4
+      return link.replace("converted/", "").replace(/\/preview\/.*/, "");
+    });
+
+    // https://deno.land/std@0.172.0/fs/mod.ts?s=exists
+    try {
+      Deno.mkdirSync(`./link/original/${name}`);
+    } catch (error) {
+      if (!(error instanceof Deno.errors.AlreadyExists)) {
+        // file already exists, ignore
+      }
+    }
+
     Deno.writeFileSync(
       `./link/original/${name}/` + file.name.split(".")[0] + ".txt",
-      new TextEncoder().encode(link.join("\n")),
+      new TextEncoder().encode(convertedLinks.join("\n")),
     );
   }
 };
