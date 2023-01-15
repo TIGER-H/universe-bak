a83b782e7e680bcb8e828c641940dfe5eb817099
Yeoreum <549747168@qq.com>
2023-01-16 01:30:21 +0800

    fix: rename subtitle

--- assemble.ts
+++ assemble.ts
@@ -7,11 +7,13 @@ for (const content of contents) {
   const filename = content.name;
 
   const mappings = JSON.parse(
-    Deno.readTextFileSync(`./link/original/${filename}/${filename}.json`)
+    Deno.readTextFileSync(`./link/original/${filename}/${filename}.json`),
   );
 
   for (const mapping of mappings) {
     const [hash, name] = Object.entries(mapping)[0];
+
+    // ......
     try {
       Deno.renameSync(
         `./link/original/${filename}/${hash}.mp4`,
@@ -36,7 +38,13 @@ for (const content of contents) {
     } catch (_e) {
       // ignore
     }
+    try {
+      Deno.renameSync(
+        `./link/original/${filename}/${hash}.vtt`,
+        `./link/original/${filename}/${name}`,
+      );
+    } catch (_e) {
+      // ignore
+    }
   }
-
 }
-
